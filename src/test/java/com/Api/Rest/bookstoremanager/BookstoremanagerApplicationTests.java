0adbe116c90b06502608c004079dae6b7c405369
LeoneCS99 <leonecezar99@gmail.com>
2022-01-18 23:56:56 -0300

    Configuração do Projeto

---
+++ src/test/java/com/Api/Rest/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Api.Rest.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
